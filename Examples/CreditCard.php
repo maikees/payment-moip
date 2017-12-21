2bad477424b4a4e64f759992c2c6a77ac0c06830
Maike Oliveira <maike@mocsolucoes.com.br>
2017-12-21 17:18:28 -0200

    Example credit card

---
+++ Examples/CreditCard.php
@@ -0,0 +1,50 @@
+<?php
+
+$moip = new \MocLibs\Payment\Moip\Moip();
+$moip->setPaymentType(\MocLibs\Payment\Moip\Moip::PaymentTypeCartao);
+$moip->setKey('1S56AIS9GBLQMNA0GGCEGUTFKC9QOVCCJNXTT6DSY'); //You key on moip
+$moip->setToken('75865XHO78OK1UTFWNJ805QOAQXH8OX5S'); //You token on moip
+$moip->setSandBox(true); //If Teste (sandbox)
+
+$moip->setProduct(new Product([
+    'nome' => 'Nome do produto.',
+    'quantidade' => 1,
+    'valor' => 500.90,
+    'detalhes' => 'Detalhes do produto.',
+]));
+
+$moip->setCreditCard(new CardCredit([
+    'numero' => '4444222233334444',
+    'nome' => 'Nome no cartão',
+    'cvc' => '233', //Cvc no verso do cartão
+    'mes' => '06', //Mês de vencimento do cartão
+    'ano' => '22', //Ano de vencimento no cartão
+    'parcelas' => '2', //Quantidade de parcelas da compra
+]));
+
+$address = new Address([
+    'endereco' => 'Rua Dom Pedro Primeiro',
+    'bairro' => 'Nova York',
+    'numero' => '49',
+    'municipio' => 'Serra',
+    'estado' => 'Espírito Santo',
+    'cep' => '29175000'
+]);
+
+$pagamento = $moip->newPayment((new Buyer([
+    'nome' => 'Nome do comprador',
+    'email' => 'atendimento@mocsolucoes.com.br',
+    'dtNascimento' => '08/06/1991', // Padrão brasileiro
+    'telefone' => '30111312', //Telefone
+    'cpf' => '58974436990',
+]))->setAddressBilling($address)
+    ->setAddressShipping($address)
+);
+
+$id = $pagamento->getId();
+$status = $pagamento->getStatus();
+
+var_dump($id);
+var_dump($status);
+var_dump($pagamento);
+?>
